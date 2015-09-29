6be62371a0929245d5ad4cf2fd9512bbbf6cf81f
Pranay <pronoy19114@gmail.com>
2015-09-29 18:42:29 +1000

    add firebase url to config file

--- client/src/js/config/config.js
+++ client/src/js/config/config.js
@@ -1,9 +1,9 @@
 var Config = {};
 
 if (process.env.NODE_ENV === 'production') {
-  Config.firebaseUrl = 'https://watch-edition-prod.firebaseIO.com';
+  Config.firebaseUrl = 'https://watch-levelup-red.firebaseIO.com';
 } else {
-  Config.firebaseUrl = 'https://watch-edition-dev.firebaseIO.com';
+  Config.firebaseUrl = 'https://watch-edition-red.firebaseIO.com';
 }
 
 module.exports = Config;
